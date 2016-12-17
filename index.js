7938b46f02b630381b9a2b0feb5937a3c3d3ef09
thinkholic <mailtokasun@gmail.com>
2016-12-17 15:29:48 +0530

    program:init

--- index.js
+++ index.js
@@ -1,31 +1,126 @@
 var program = require('commander');
+var prompt = require('prompt');
+var gitClone = require('nodegit-clone');
+var fs = require('fs');
+var mysql = require('mysql');
 
-program
-  .command('init [dir]')
-  .description('Initializing...')
-  .action(function(dir) {
-    console.log('[wp init %s]', dir);
-    if (dir) {
-      console.log('CREATE a directory named %s and chnage into that;', dir);
+var env = {
+  wpRemoteSrc: {
+    url: 'git@github.com:CodeOcee/Yii-Project.git'
+  },
+  config: {
+    file: 'config.json'
+  }
+};
+
+// prompt schema
+var promptSchema = {
+  properties: {
+    site_name: {
+      description: 'Enter the site name'
+    },
+    mysql_db_host: {
+      description: 'MYSQL Database Host'
+    },
+    mysql_db_user: {
+      description: 'MYSQL Database User'
+    },
+    mysql_db_password: {
+      description: 'MYSQL Database Password'
+    },
+    mysql_db_name: {
+      description: 'MYSQL Database Name'
+    },
+    mysql_db_tbl_prefix: {
+      description: 'MYSQL Database Table Prefix'
     }
-    console.log('GET user inputs; site_name, db_host, db_user, db_pwd, db_name, db_tbl_prefix, tmpl_name');
-    console.log('CREATE `config.json` file according to those user inputs');
-    console.log('RUN generator script:');
-    console.log('INSTALL WordPress latest version (CLONE from remote repository,');
-    console.log('CREATE MySQL Database,');
-    console.log('UPDATE config.php and other WordPress files accordingly.');
+  }
+};
+
+// program: init
+program
+  .command('init <dir>')
+  .description('wp init <dir>')
+  .action(function(dir) {
+    if (dir) {
+      // prompt for user inputs
+      prompt.start();
+      prompt.get(promptSchema, function (err, config) {
+        if (err) throw err;
+        console.log(config);
+
+        // get latest WordPress installation
+        // via cloning the remote repository
+        console.log('cloning...')
+        gitClone({
+          url: env.wpRemoteSrc.url,
+          localPath: dir
+        }).then(function(repo) {
+          console.log('cloned.');
+
+          // create config.json
+          var configFilePath = dir+'/'+env.config.file;
+          var configJSON = {
+            config: {
+              ste_name: config.site_name,
+              mysql_db_host: config.mysql_db_host,
+              mysql_db_user: config.mysql_db_user,
+              mysql_db_password: config.mysql_db_password,
+              mysql_db_name: config.mysql_db_name,
+              mysql_db_tbl_prefix: config.mysql_db_tbl_prefix
+            }
+          };
+          fs.writeFile(configFilePath, JSON.stringify(configJSON), function(err) {
+            if (err) throw err;
+            console.log('config.json created.');
+            // [TODO] format config.json
+          });
+
+          // [TODO] Update config.php
+        });
+
+        // create MySQL Database
+        console.log('DB creating...');
+        var dbConn = mysql.createConnection({
+          host : config.mysql_db_host,
+          user : config.mysql_db_user,
+          password : config.mysql_db_password
+        });
+        dbConn.connect();
+        dbConn.query('CREATE DATABASE '+config.mysql_db_name, function(err, res) {
+          if (err) throw err;
+          console.log('DB created... '+config.mysql_db_name);
+        });
+        dbConn.end();
+      });
+    }
   });
 
+// program: config
 program
   .command('config <key> [val]')
-  .description('')
+  .description('wp config <key> [val]')
   .option('-u, --update_mode')
   .action(function(key, val, options) {
-    console.log('[wp config %s %s %s]', options.update_mode, key, val);
     if (options.update_mode) {
-      console.log('UPDATE config.json with %s = %s', key, val);
+      // get current config
+      // [TODO] [TEMP] Here, we use temp `dir` name for development purpose
+      // later it must be remove and corrected.
+      var configFilePath = 'dir/'+env.config.file;
+      fs.readFile(configFilePath, 'utf8', (err, config) => {
+        if (err) throw err;
+        var configJSON = JSON.parse(config);
+        configJSON.config[key] = val;
+
+        fs.writeFile(configFilePath, JSON.stringify(configJSON), function(err) {
+          if (err) throw err;
+          console.log('config.json updated.');
+          // [TODO] format config.json
+        });
+
+      });
     } else {
-      console.log('--update_mode flag need to be enable for running this command.')
+      console.log('\r\n  error: missing required flag `--update_mode`\r\n');
     }
   });
 
